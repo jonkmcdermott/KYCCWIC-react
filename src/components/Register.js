a7a442a70fa517c7b5005213b68211782e1849a7
jonkmcdermott <jonkmcdermott@gmail.com>
2022-09-28 16:35:40 -0700

    updated register prompt

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,7 +5,7 @@ function Register() {
   return (
     <div id='register'>
         <div className="container">
-            <h1><a className='external-link' href='https://www.surveymonkey.com/r/THWXYC8' target='_blank' rel="noreferrer">Register for the Celebration with SurveyMonkey!</a></h1>
+            <h1><a className='external-link' href='https://www.surveymonkey.com/r/THWXYC8' target='_blank' rel="noreferrer">Click Here to Register!</a></h1>
             <p className="text-block">
                 <strong>All genders welcome to attend</strong> <br></br>
                 <strong>Registration through <a className='external-link' href='https://www.surveymonkey.com/r/THWXYC8' target='_blank' rel="noreferrer">SurveyMonkey</a> </strong> <br></br>
