21607807627c9f120cb32d1f8da94719ca1048e6
jonkmcdermott <jonkmcdermott@gmail.com>
2022-11-01 15:21:58 -0700

    added 2021 feedback

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Register from './components/Register';
 import BreakoutSessions from './components/BreakoutSessions';
 import EmploymentPanel from './components/EmploymentPanel';
 import Keynote from './components/Keynote';
+import Testimonial from './components/Testimonial';
 
 
 
@@ -23,6 +24,7 @@ function App() {
     <div className="App">
       <Navbar />
       <LoadScreen />
+      <Testimonial />
       <ThemeHeading />
       <About />
       <EventCommittee />
---
+++ src/components/Testimonial.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Testimonial.css'
+
+function Testimonial() {
+  return (
+    <div className='container'>
+        <div className="testimonial-block">
+            <p className="testimonial" id="testimonial-one">“Absolutely fantastic IT, business, and general employment advice and recommendations. This could not have gone better.”</p>
+            <p className="testimonial" id="testimonial-two">“It is truly wonderful to connect across technology with other areas of the state, or in my case a different state. More programs like this should be created as the information is so important to share.”</p>
+            <p className="testimonial" id="testimonial-three">“Aside from the program development aspects and flow, your presenters were knowledgeable, fun, interesting, and made me aware of programs that can translate into partnerships or similar offerings in our area.”</p>
+            <p className="testimonial" id="testimonial-four">“Great information across a broad spectrum of topics”</p>
+            <p className="testimonial" id="testimonial-five">“There is a lot of information out there that I am not aware of. It was great conference and looking forward to the next one.”</p>
+            <p className="testimonial" id="testimonial-six">“I had never heard of this conference before, but definitely felt it was student-centered, and carried tremendous energy!”</p>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonial
