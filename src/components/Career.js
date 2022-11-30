2333c1f772b5226531b7274811da9fc0f395835e
jonkmcdermott <jonkmcdermott@gmail.com>
2022-11-30 14:50:42 -0800

    updated career fair page

--- src/components/Career.js
+++ src/components/Career.js
@@ -31,11 +31,124 @@ function Career() {
                 {/* Individual Career Participants go here and formating is handled by CSS */}
 
                 {DisplayCareerBadge(
+                    'BGMU',
+                    'https://www.bgmu.com/',
+                    true,
+                    './assets/CareerFair/bgmu.png'
+                )}
+
+                {DisplayCareerBadge(
+                    'Cengage',
+                    'https://www.cengage.com/',
+                    true,
+                    './assets/Sponsors/Cengage.png'
+                )}
+
+                {DisplayCareerBadge(
+                    'Coretech',
+                    'https://www.coretechllc.com/',
+                    true,
+                    './assets/Sponsors/Coretech.png'
+                )}
+
+                {DisplayCareerBadge(
                     'Dollar General',
                     'https://careers.dollargeneral.com/',
                     true,
                     './assets/CareerFair/dollargeneral.png'
                 )}
+
+                {DisplayCareerBadge(
+                    'FORVIS',
+                    'https://www.forvis.com',
+                    true,
+                    './assets/Sponsors/Forvis.png'
+                )}
+
+                {DisplayCareerBadge(
+                    'Franklin Banking & Trust',
+                    'https://www.fbtco.com/',
+                    true,
+                    './assets/CareerFair/franklin.png'
+                )}                
+
+                {DisplayCareerBadge(
+                    'German American Bank',
+                    'https://germanamerican.com/personal/',
+                    true,
+                    './assets/Sponsors/GermanAmerican.jpg'
+                )}
+
+                {DisplayCareerBadge(
+                    'Graves Gilbert',
+                    'https://www.gravesgilbert.com/',
+                    true,
+                    './assets/CareerFair/gravesgilbert.jpg'
+                )}  
+
+                {DisplayCareerBadge(
+                    'Houchens Industries',
+                    'https://houchens.com/',
+                    true,
+                    './assets/CareerFair/houchens.png'
+                )}  
+
+                {DisplayCareerBadge(
+                    'Lang Company',
+                    'https://www.langcompany.com/',
+                    true,
+                    './assets/Sponsors/Lang.png'
+                )}
+
+                {DisplayCareerBadge(
+                    'McGraw Hill',
+                    'https://www.mheducation.com',
+                    true,
+                    './assets/Sponsors/MH.jpg'
+                )}
+
+                {DisplayCareerBadge(
+                    'Medical Center Health',
+                    'https://medcenterhealth.org/',
+                    true,
+                    './assets/CareerFair/medcenter.jpg'
+                )} 
+
+                {DisplayCareerBadge(
+                    'RehabBoost',
+                    'https://rehabboost.com/',
+                    true,
+                    './assets/Sponsors/RehabBoost.png'
+                )}
+
+                {DisplayCareerBadge(
+                    'Service One Credit Union',
+                    'https://www.socu.com',
+                    true,
+                    './assets/Sponsors/SOCU.png'
+                )}   
+
+                {DisplayCareerBadge(
+                    'Southcentral Kentucky Community & Technical College',
+                    'https://southcentral.kctcs.edu/',
+                    true,
+                    './assets/Sponsors/SKYCTC.png'
+                )}
+
+                {DisplayCareerBadge(
+                    'Sumitomo Electronics Wiring',
+                    'https://sewsus.com/careers/',
+                    true,
+                    './assets/CareerFair/sumitomo.png'
+                )} 
+
+                {DisplayCareerBadge(
+                    'Western Kentucky University Gordon Ford College of Business',
+                    'https://www.wku.edu/business',
+                    true,
+                    './assets/Sponsors/WKU.png'
+                )}               
+
 
 
             </div>
